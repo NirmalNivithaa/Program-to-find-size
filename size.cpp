e1a334e225724dfa708e8d20c2e8a7bd6e57c6e6
NirmalNivithaa <92011024+NirmalNivithaa@users.noreply.github.com>
2021-10-06 12:00:32 +0530

    created size.cpp
    This file prints the size of data type

---
+++ size.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    int b;
+    float c;
+    double d;
+    cout<<"\n Size of char="<<sizeof(a);
+    cout<<"\n Size of int="<<sizeof(b);
+    cout<<"\n Size of float="<<sizeof(c);
+    cout<<"\n Size of double="<<sizeofdouble(d);
+}
